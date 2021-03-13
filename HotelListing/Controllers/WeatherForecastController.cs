00502c9586faaf9809de899f659515e7db2d2ea8
IlyaTheCreator <respondatanytime@gmail.com>
2021-03-13 09:18:47 +0300

    Initial commit

--- HotelListing/Controllers/WeatherForecastController.cs
+++ HotelListing/Controllers/WeatherForecastController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+// This is my amazing application!
 namespace HotelListing.Controllers
 {
     [ApiController]
